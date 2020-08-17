f126dce73f7d5f6e56970eb983f9a72d0bf8802d
Kapthas59 <68538834+Kapthas59@users.noreply.github.com>
2020-08-17 18:05:16 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,6 +95,7 @@ $ajaxUtils.sendGetRequest(
         // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
+        console.log(categories);
         var chosenCategoryShortName = "'" + chooseRandomCategory('categories') + "'";
         chosenCategoryShortName = chosenCategoryShortName.toUpperCase();
         console.log(chosenCategoryShortName);
