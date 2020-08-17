695fb9f60eb1ed18ab445bbb660351566b852c14
Kapthas59 <68538834+Kapthas59@users.noreply.github.com>
2020-08-17 17:32:24 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -96,6 +96,8 @@ $ajaxUtils.sendGetRequest(
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
         var chosenCategoryShortName = "'" + chooseRandomCategory('categories') + "'";
+        chosenCategoryShortName = chosenCategoryShortName.toUpperCase();
+        console.log(chosenCategoryShortName);
 
 
         // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -109,7 +111,7 @@ $ajaxUtils.sendGetRequest(
         // Hint: you need to surround the chosen category short name with something before inserting
         // it into the home html snippet.
         //
-        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName.toUpperCase());
+        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
         // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
